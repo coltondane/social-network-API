e59eeac3533b1ad0dcc3925e696f2447e4be14d8
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-07 15:18:43 -0700

    added put to update a users information

--- Develop/controllers/userController.js
+++ Develop/controllers/userController.js
@@ -1,7 +1,7 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-    // get all users
+  // get all users
   async getUsers(req, res) {
     try {
       const users = await User.find();
@@ -10,19 +10,21 @@ module.exports = {
       res.json(error);
     }
   },
-// get a single user
+  // get a single user
   async getSingleUser(req, res) {
     try {
-        const user = await User.findOne({ _id: req.params.userId })
-        .select('-__v');
-        // check to make sure the user exists
-        if(!user) {
-            return res.status(404).json({message: 'This user does not currently exist'})
-        }
-        // log data
-        res.json(user);
+      const user = await User.findOne({ _id: req.params.userId })
+      .select("-__v");
+      // check to make sure the user exists
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "This user does not currently exist" });
+      }
+      // log data
+      res.json(user);
     } catch (error) {
-        res.json(error);
+      res.json(error);
     }
   },
   // creating a user
@@ -34,23 +36,46 @@ module.exports = {
       res.json(error);
     }
   },
-//   delete a user and their thought
+  //   update user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body},
+        { runValidators: true, new: true }
+        );
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "This user does not currently exist" });
+      }
+      // log data
+      res.json(user);
+    } catch (error) {
+      res.json(error);
+    }
+  },
+  //   delete a user and their thought
   async deleteUser(req, res) {
     try {
-        // remove user
-        const user = await User.findOneAndRemove({ _id: req.params.userId });
-        if(!user) {
-            return res.status(404).json({message: 'This user does not currently exist'})
-        }
+      // remove user
+      const user = await User.findOneAndRemove({ _id: req.params.userId });
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: "This user does not currently exist" });
+      }
 
-        // remove thought
-        const thought = await Thought.deleteMany({ username: user.username });
-        if(!thought) {
-            return res.status(404).json({message: 'This user does not have any thoughts to delete'})
-        }
-        res.json({ message: 'the user and their thought(s) have been deleted'})
+      // remove thought
+      const thought = await Thought.deleteMany({ username: user.username });
+      if (!thought) {
+        return res
+          .status(404)
+          .json({ message: "This user does not have any thoughts to delete" });
+      }
+      res.json({ message: "the user and their thought(s) have been deleted" });
     } catch (error) {
-        res.json(error);
+      res.json(error);
     }
-  }
+  },
 };
--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -1,17 +1,18 @@
 const router = require('express').Router();
 // imports
 const { 
+    createUser,
     getUsers,
     getSingleUser,
+    updateUser,
     deleteUser,
-    createUser,
 } = require('../../controllers/userController');
 
 // routes
 // http://localhost:3001/api/users
 router.route('/').get(getUsers).post(createUser);
 // http://localhost:3001/api/users/:(userId)
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 
 

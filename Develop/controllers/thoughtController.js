49ec329961f95aff92e94ccd2bac78bf80f0ef2f
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-09 21:45:55 -0700

    un-modularizing to fix route error

--- Develop/controllers/thoughtController.js
+++ Develop/controllers/thoughtController.js
@@ -54,5 +54,48 @@ module.exports = {
         } catch (error) {
             res.status(500).json(error);
         }
-    }
+    },
+    // Add a reaction to a thought
+    async addReaction(req, res) {
+        console.log('You are adding a reaction');
+        console.log(req.body);
+    
+        try {
+          const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { runValidators: true, new: true }
+          );
+    
+          if (!thought) {
+            return res
+              .status(404)
+              .json({ message: 'No reaction found with that ID' });
+          }
+    
+          res.json(thought);
+        } catch (error) {
+          res.status(500).json(error);
+        }
+      },
+      // Delete reaction from thought
+      async deleteReaction(req, res) {
+        try {
+          const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { runValidators: true, new: true }
+          );
+    
+          if (!thought) {
+            return res
+              .status(404)
+              .json({ message: 'No reaction found with that ID' });
+          }
+    
+          res.json(student);
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      },
 }
--- Develop/routes/api/thoughtRoutes.js
+++ Develop/routes/api/thoughtRoutes.js
@@ -5,6 +5,8 @@ const {
     getSingleThought,
     updateThought,
     createThought,
+    addReaction,
+    deleteReaction
 } = require('../../controllers/thoughtController');
 
 // routes
@@ -12,5 +14,10 @@ const {
 router.route('/').get(getThoughts).post(createThought);
 // http://localhost:3001/api/thoughts/(:thoughtId)
 router.route('/:thoughtId').get(getSingleThought).put(updateThought);
+// http://localhost:3001/api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction)
+// http://localhost:3001/api/thoughts/:thoughtId/reactions/reactionId
+router.route('/:thoughtId/reactions/reactionId').delete(deleteReaction);
+
 
 module.exports = router;
